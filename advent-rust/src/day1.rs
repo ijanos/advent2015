be7c5f51c000328553a11e0323c8314e10ab97fe
János Illés <ijanos@gmail.com>
2016-01-04 20:15:35 +0100

    day 1 part 2 rust solution

--- advent-rust/src/day1.rs
+++ advent-rust/src/day1.rs
@@ -3,8 +3,7 @@ use std::io::BufReader;
 use std::io::BufRead;
 
 fn part1() -> i32 {
-    let f = File::open("input.txt").unwrap();
-    let file = BufReader::new(&f);
+    let file = BufReader::new(File::open("input.txt").unwrap());
     let mut floor:i32 = 0;
     for line in file.lines() {
         for c in line.unwrap().chars() {
@@ -18,8 +17,27 @@ fn part1() -> i32 {
     floor
 }
 
-fn part2() {}
+fn part2() -> Option<i32> {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+    let mut floor :i32 = 0;
+    let mut n :i32 = 1;
+    for line in file.lines() {
+        for c in line.unwrap().chars() {
+            match c.to_string().as_ref() {
+                "(" => floor += 1,
+                ")" => floor -= 1,
+                _ => panic!()
+            }
+            if floor == -1 {
+                return Some(n);
+            }
+            n += 1;
+        }
+    }
+    None
+}
 
 pub fn main() {
     println!("part 1: {}", part1());
+    println!("part 2: {}", part2().unwrap());
 }
