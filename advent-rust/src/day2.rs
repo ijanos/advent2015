b97c507c48e41cfec88f3ef628647ff64480e950
János Illés <ijanos@gmail.com>
2016-01-10 14:48:39 +0100

    day 2 part 2 rust solution

--- advent-rust/src/day2.rs
+++ advent-rust/src/day2.rs
@@ -3,7 +3,8 @@ use std::io::prelude::*;
 
 pub fn main() {
     let stdin = io::stdin();
-    let mut result = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
     for line in stdin.lock().lines() {
         let line = line.unwrap();
         let split = line.split("x");
@@ -12,7 +13,9 @@ pub fn main() {
         let area1 = nums[0] * nums[1];
         let area2 = nums[0] * nums[2];
         let area3 = nums[1] * nums[2];
-        result += area1 * 2 + area2 * 2 + area3 * 2 + area1;
+        part1 += area1 * 2 + area2 * 2 + area3 * 2 + area1;
+        part2 += nums[0] * 2 + nums[1] * 2 + nums[0] * nums[1] * nums[2];
     }
-    println!("Part1: {}", result);
+    println!("Part1: {}", part1);
+    println!("Part2: {}", part2);
 }
