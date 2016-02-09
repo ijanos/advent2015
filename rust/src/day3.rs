0d5a59e33f0377125687ff3008823470599ce070
János Illés <ijanos@gmail.com>
2016-02-09 07:34:36 +0100

    simplify day 03 rust code
    use more structs and less ifs.

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -1,26 +1,37 @@
 use std::io;
 use std::io::prelude::*;
 use std::collections::HashSet;
+use std::mem;
 
 struct Santa {
     x: i32,
     y: i32
 }
 
-fn step(c: char, s: &mut Santa) {
-    match c {
-        '>' => s.x += 1,
-        '<' => s.x -= 1,
-        '^' => s.y += 1,
-        'v' => s.y -= 1,
-        _ => panic!()
+struct Day03 {
+    part1: Santa,
+    part2: Santa,
+    temp:  Santa
+}
+
+impl Santa {
+    fn step(&mut self, c: char) {
+        match c {
+            '>' => self.x += 1,
+            '<' => self.x -= 1,
+            '^' => self.y += 1,
+            'v' => self.y -= 1,
+            _ => panic!()
+        }
     }
 }
 
 pub fn main() {
-    let mut p1_santa = Santa {x: 0, y: 0};
-    let mut p2_santa = Santa {x: 0, y: 0};
-    let mut p2_robo  = Santa {x: 0, y: 0};
+    let mut day03 = Day03 {
+        part1: Santa {x: 0, y: 0},
+        part2: Santa {x: 0, y: 0},
+        temp:  Santa {x: 0, y: 0}
+    };
 
     let mut part1_visited = HashSet::new();
     let mut part2_visited = HashSet::new();
@@ -28,23 +39,16 @@ pub fn main() {
     part1_visited.insert((0,0));
     part2_visited.insert((0,0));
 
-    let mut roboturn: bool = false;
-
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-      let line = line.unwrap();
-      for c in line.chars() {
-            step(c, &mut p1_santa);
-            if roboturn {
-                step(c, &mut p2_robo);
-            } else {
-                step(c, &mut p2_santa);
-            }
-            roboturn = !roboturn;
-            part1_visited.insert((p1_santa.x, p1_santa.y));
-            part2_visited.insert((p2_santa.x, p2_santa.y));
-            part2_visited.insert((p2_robo.x, p2_robo.y));
-      }
+        let line = line.unwrap();
+        for c in line.chars() {
+              day03.part1.step(c);
+              day03.part2.step(c);
+              part1_visited.insert((day03.part1.x, day03.part1.y));
+              part2_visited.insert((day03.part2.x, day03.part2.y));
+              mem::swap(&mut day03.part2, &mut day03.temp)
+        }
     }
     println!("Part 1: {}", part1_visited.len());
     println!("Part 2: {}", part2_visited.len());
