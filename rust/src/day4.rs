d0cbc64d386d5f3895c5afeeb73b6b05eebe0aad
János Illés <ijanos@gmail.com>
2016-02-10 07:34:56 +0100

    day 4 rust solution

---
+++ rust/src/day4.rs
@@ -0,0 +1,20 @@
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+const INPUT: &'static str = "bgvyzdsv";
+
+fn first_n_zeroes(zeroes: usize) -> usize {
+    let mut md5hasher = Md5::new();
+    (0..).find(
+        |&i| {
+            md5hasher.reset();
+            md5hasher.input_str(&format!("{}{}", INPUT, i));
+            md5hasher.result_str().chars().take(zeroes).all(|c| c == '0')
+        }
+    ).unwrap()
+}
+
+pub fn main() {
+    println!("Part 1: {}", first_n_zeroes(5));
+    println!("Part 2: {}", first_n_zeroes(6));
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,9 @@
+extern crate crypto;
+
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 use std::env;
 
@@ -13,6 +16,7 @@ fn main() {
             "1" => day1::main(),
             "2" => day2::main(),
             "3" => day3::main(),
+            "4" => day4::main(),
             _ => println!("No such day")
         }
     }
